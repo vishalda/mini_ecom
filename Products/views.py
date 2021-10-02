0c20974856c14aabc35117cc5e61d5dc092e0ab8
vishalda <vdavishu@gmail.com>
2021-10-02 23:20:02 +0530

    :tada: Added Parallax, Footer

--- Products/views.py
+++ Products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Products
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    products=Products.objects.all()
+    data={'products':products}
+    return render(request, 'index.html',data)
