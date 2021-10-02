b431778373b734e87a07cbcfe6bffcf064c0cdab
vishalda <vdavishu@gmail.com>
2021-10-02 08:15:23 +0530

    :rocket: Added Product data

--- Products/models.py
+++ Products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    image=models.ImageField(upload_to="images")
+    title=models.CharField(max_length=50)
+    description=models.TextField()
+    price=models.CharField(max_length=20)
+    def __str__(self):
+        return self.title
