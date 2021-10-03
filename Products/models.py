681a4d924986bcb701968d7a8703bbb46138c19c
vishalda <vdavishu@gmail.com>
2021-10-03 15:07:17 +0530

    :racehorse: Added Card design

--- Products/models.py
+++ Products/models.py
@@ -6,5 +6,6 @@ class Products(models.Model):
     title=models.CharField(max_length=50)
     description=models.TextField()
     price=models.CharField(max_length=20)
+    offer=models.CharField(max_length=10,default='50')
     def __str__(self):
         return self.title
